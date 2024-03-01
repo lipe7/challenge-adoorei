a60fad4ec65eb38b986bb1a38e2673b41a6aa2da
felipe <felipec165@gmail.com>
2024-03-01 04:11:20 -0300

    test: return struct list sales test

--- tests/Feature/SaleTest.php
+++ tests/Feature/SaleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Domain\Product\Product;
+use App\Domain\Sale\Sale;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -20,5 +21,27 @@ class SaleTest extends TestCase
                 'errors'
             ])
             ->assertJsonValidationErrors(['products']);
+    }
+
+    /** @test */
+    public function it_returns_sales_json_structure()
+    {
+        $response = $this->get('/api/sales?order_by=sales.sale_id&order_by_type=desc&per_page=5');
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data',
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'links',
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total'
+        ]);
     }
 }
