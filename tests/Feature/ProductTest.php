d4cef20c616127e89d17c92512c567a76ba45260
felipe <felipec165@gmail.com>
2024-03-02 03:17:12 -0300

    test: get all products

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -4,32 +4,21 @@ namespace Tests\Feature;
 
 use App\Domain\Product\Product;
 use App\Domain\Product\ProductService;
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Mockery;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_can_list_available_products()
     {
+        ProductFactory::new()->create();
+
         $response = $this->get('/api/products');
-
-        $mockRepository = Mockery::mock('App\Domain\Product\ProductRepository');
-        $mockRepository->shouldReceive('allAvailable')->andReturn(
-            new Collection([
-                new Product(['name' => 'Celular 1', 'price' => '1800.00', 'description' => 'Lorenzo Ipsulum']),
-                new Product(['name' => 'Celular 2', 'price' => '3200.00', 'description' => 'Lorem ipsum dolor']),
-            ])
-        );
-
-        $productService = new ProductService($mockRepository);
-
-        $products = $productService->getAvailableProducts();
-
-        $this->assertCount(2, $products);
-        $this->assertEquals('Celular 1', $products[0]->name);
-        $this->assertEquals('1800.00', $products[0]->price);
-        $this->assertEquals('Lorenzo Ipsulum', $products[0]->description);
 
         $response->assertStatus(200);
     }
